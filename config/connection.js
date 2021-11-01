f9391d7d9c91acc625ae3393124245beaaf4bb82
jazzmbarry <jazzmbarry@gmail.com>
2021-10-31 21:22:15 -0600

    adding all functionality for server.js to work

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
-const Sequelize = require('sequelize')
-
-require('dotenv').config()
+// create connection to our database
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+// import Dotenv for loading env variables
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.BD_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3001
-    })
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: "localhost",
+    dialect: "mysql",
+    dialectOtions: {
+        decimalNumbers: true,
+    },
+});
 
-module.exports = sequelize
+module.exports = sequelize;
